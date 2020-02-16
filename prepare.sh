dc356fa1912abecc86956fca708332258a68b56c
herrick <1091168853@qq.com>
2020-02-16 15:20:32 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -10,7 +10,7 @@ npm install --save-dev webpack
 npm install --save-dev webpack-cli
 
 # install depandance
-npm install @babel/core @babel/plugin-transform-runtime @babel/preset-env @babel/preset-react babel-loader css-loader file-loader fs html-webpack-plugin react react-dom style-loader
+npm install @babel/core @babel/plugin-transform-runtime @babel/preset-env @babel/preset-react babel-loader css-loader file-loader fs html-webpack-plugin react react-dom style-loader --save-dev
 
 # pack
 webpack
